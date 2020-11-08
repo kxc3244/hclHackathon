84dccdbc604bae4c618af6cebbbe4ad402f583ea
Imad Yousuf <imad_yousuf@hotmail.com>
2020-11-08 13:46:20 -0500

    Adding service and repository layer

---
+++ eMobileConnect/src/main/java/com/mobileconnect/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.mobileconnect.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mobileconnect.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+}
---
+++ eMobileConnect/src/main/java/com/mobileconnect/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.mobileconnect.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+	public CustomerService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
